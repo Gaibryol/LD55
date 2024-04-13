140acbaf71eeea572ea440e09caaeda281738036
BhanHG <96505078+BhanHG@users.noreply.github.com>
2024-04-13 13:45:10 -0600

    Added a ScoreSystem.
    Keeps track off number of this and the score.
    Need to add 2 event listener before functional.

--- Assets/GameAssets/Scripts/Constants/Constants+Game.cs
+++ Assets/GameAssets/Scripts/Constants/Constants+Game.cs
@@ -18,5 +18,10 @@ public partial class Constants
 		public static Vector3 DownExtraDistance = new Vector3(0, 0.5f, 0);
 		public static Vector3 LeftExtraDistance = new Vector3(0.5f, 0, 0);
 		public static Vector3 RightExtraDistance = new Vector3(-0.5f, 0, 0);
+
+		//Points for hits
+		public static int PerfectHit = 100;
+		public static int OkayHit = 50;
+		public static int BadHit = 25;
 	}
 }
