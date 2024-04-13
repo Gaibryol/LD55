2fc45f9ccdace1a4246d91d1f64ec4dcea37abe2
BhanHG <96505078+BhanHG@users.noreply.github.com>
2024-04-13 00:29:50 -0600

    Added AudioSystem, AudioEvents, and Constants+Audio.

---
+++ Assets/GameAssets/Constants/Constants+Audio.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public partial class Constants
+{
+	public class Audio
+	{
+		public const string MusicVolumePP = "Music";
+		public const string SFXVolumePP = "SFX";
+
+		public const float DefaultMusicVolume = 0.15f;
+		public const float DefaultSFXVolume = 0.45f;
+
+		public const float MusicFadeSpeed = 0.1f;
+
+		public class Music
+		{
+			public const string GameTheme = "GameTheme";
+			public const string MainMenuTheme = "MainMenuTheme";
+		}
+
+		public class SFX
+		{
+
+		}
+	}
+}
