488a8164953729f99466e1acc84fef712bf26824
BhanHG <96505078+BhanHG@users.noreply.github.com>
2024-04-12 23:18:22 -0600

    Added a MidiConverter.
    Converts the json Midi file to Notes to be added to leftQueue, upQueue, rightQueue, downQueue.

---
+++ Assets/GameAssets/Scripts/Midi/Note.cs
@@ -0,0 +1,13 @@
+
+[System.Serializable]
+public class Note
+{
+    public string name;
+    public float time;
+    
+    public Note(string name, float time)
+    {
+        this.name = name;
+        this.time = time;
+    }
+}
---
+++ Assets/GameAssets/Scripts/Midi/Notes.cs
@@ -0,0 +1,5 @@
+[System.Serializable]
+public class Notes
+{
+    public Note[] notes;
+}
